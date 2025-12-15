0b92890b790e508425932685bda1faaa9e51e5a8
Marcos-UTFPR <marcosviniciussilva@alunos.utfpr.edu.br>
2025-12-15 11:26:45 -0300

    Feat: Interface melhorada e adicionado responsividade.

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'My_First_Flutter_App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
         ),
         home: MyHomePage(),
       ),
@@ -34,29 +34,162 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  // ↓ Add the code below.
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;     // ← Add this property.
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 600,  // ← Here.
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,    // ← Change to this.
+                  onDestinationSelected: (value) {
+        
+                    // ↓ Replace print with this.
+                    setState(() {
+                      selectedIndex = value;
+                    });
+        
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,  // ← Here.
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;                 // ← Add this.
 
-    return Scaffold(
-      body: Column(
+    // ↓ Add this.
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,  // ← Add this.
         children: [
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
-
+          //Text(appState.current.asLowerCase),   // ← Change this...
+          BigCard(pair: pair),                // ← ... to this, creating a separate widget that doesn't interact with the whole state
+          SizedBox(height: 10), // O widget SizedBox só ocupa espaço e não renderiza nada, usado para criar "lacunas" visuais.
           // ↓ Add this.
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-              appState.getNext();  // ← This instead of print().
-            },
-            child: Text('Next'),
-          ),
-
+          Row(
+            mainAxisSize: MainAxisSize.min,   // ← Add this.
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  print('button LIKE pressed!');
+                appState.toggleFavorite();  // ← This instead of print().
+                },
+                //child: Text('❤ Like'),
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('button NEXT pressed!');
+                  appState.getNext();  // ← This instead of print().
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),      
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);       // ← Add this.
+    // ↓ Add this.
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,    // ← And also this.
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        // ↓ Change this line.
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
